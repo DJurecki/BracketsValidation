cce1eab660d208e7b966ce88691ce8e5abc67f4d
Dejwu <dejwux1@gmail.com>
2019-10-15 10:42:07 +0200

    Moj pierwszy projekt

---
+++ src/com/jurecki/Main.java
@@ -0,0 +1,27 @@
+package com.jurecki;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        String str = "())((())))";
+        int firstCounter = 0;
+        int secondCounter = 0;
+        for (int i = 0 ; i != str.length() ; i++){
+            char c = str.charAt(i);
+            if(c == '('){
+                firstCounter++;
+            }
+            if(c == ')'){
+                secondCounter++;
+            }
+            if (secondCounter > firstCounter){
+                System.out.println("Not good!");
+                break;
+            }
+        }
+        System.out.println("Tyle '(': " + firstCounter + " i tyle ')': " + secondCounter);
+
+
+    }
+}
